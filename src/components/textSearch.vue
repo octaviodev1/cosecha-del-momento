a81473f6fc399f9a63d5d299628e899fbf6be079
Brais Vila <braisvial96@hotmail.com>
2023-10-10 14:00:15 +0200

    Creación del input "busqueda por texto"

--- src/components/textSearch.vue
+++ src/components/textSearch.vue
@@ -1,1 +1,24 @@
-v
+<template>
+    <div class="relative m-3" data-te-input-wrapper-init>
+        <input type="text" class="peer block min-h-[auto] w-fitcontent rounded border-2
+    border-gray bg-green px-3 py-[0.32rem] 
+      leading-[1.6] outline-none transition-all duration-200 ease-linear" id="movePlaceholder" />
+        <label for="movePlaceholder" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%]
+      origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-white
+      transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem]
+      peer-focus:scale-[0.8] peer-focus:text-primary 
+      peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
+      peer-data-[te-input-state-active]:scale-[0.8] 
+      motion-reduce:transition-none ">
+            Busca tu fruta o verdura
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'black': '#000000',
+      'red':'#d94052',
+      'orange':'#ee7e4c',
+      'yellow':'#ead56c',
+      'green':'#0a996f',
+      'gray': '#898b75',
+    },
     extend: {},
   },
   plugins: [],
