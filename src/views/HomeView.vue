5e7832fce2d1cc27fd6162e9e6cca40d227cc8e1
Octavian Badea <octavio.badea@gmail.com>
2023-10-12 12:55:25 +0200

    feature/FooterInfo: Se ha completado provisionalmente el footer de la web. Se queda pendiente de nuevos cambios.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,19 @@
   <div class="flex justify-end m-5">
     <TextSearch @search-text-updated="searching"></TextSearch>
   </div>
+  <FooterInfo></FooterInfo>
 </template>
 
 <script>
 import TopNav from "@/components/TopNav.vue";
 import TextSearch from '../components/TextSearch.vue';
+import FooterInfo from "../components/FooterInfo.vue";
 
 export default {
   components: {
     TextSearch,
     TopNav,
-
+    FooterInfo
   },
   methods: {
     searching(text) {
