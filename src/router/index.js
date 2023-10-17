515db110bbf3dd01372dc3bf84f6b8d688a6096e
Octavian Badea <octavio.badea@gmail.com>
2023-10-17 10:56:47 +0200

    Fix/router Se ha convertido la ruta de about en una avanzada para usar el lazy loading.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CatalogueView from "../views/CatalogueView.vue";
-import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,7 +18,7 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      component: AboutView,
+      component: () => import("../views/AboutView.vue"),
     },
   ],
 });
