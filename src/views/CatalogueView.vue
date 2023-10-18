6f16d967e5517296e5948de60bf17581e42de235
Octavian Badea <octavio.badea@gmail.com>
2023-10-18 12:56:41 +0200

    Feature/database Se ha incluido una base de datos, junto con una peticion y una pequeña introduccion en los componentes CatalogueContent.vue y CatalogueView.vue. Se ha incluido una netlify function para traer datos de la base de datos.

--- src/views/CatalogueView.vue
+++ src/views/CatalogueView.vue
@@ -1,8 +1,11 @@
 <template>
   <TopNav></TopNav>
-  <div>
+  <div class="flex flex-row">
     <div class="flex flex-col w-[28rem] justify-start ml-4 mb-4">
       <FiltersSection />
+    </div>
+    <div>
+      <CatalogueContent />
     </div>
   </div>
   <FooterInfo></FooterInfo>
@@ -12,12 +15,14 @@
 import TopNav from "@/components/TopNav.vue";
 import FiltersSection from "../components/FiltersSection.vue";
 import FooterInfo from "../components/FooterInfo.vue";
+import CatalogueContent from "../components/CatalogueContent.vue";
 
 export default {
   components: {
     TopNav,
     FiltersSection,
     FooterInfo,
+    CatalogueContent
   },
   methods: {
     searching(text) {
