05364c67eb35627fc2f6e7288151de5cdbb33d5f
Brais Vila <braisvial96@hotmail.com>
2023-10-22 18:33:21 +0200

    Modificadas las views de Términos de Servicios y Política de Privacidad

--- src/views/PolicyView.vue
+++ src/views/PolicyView.vue
@@ -1,55 +1,53 @@
 <template>
-    <div>Política de Privacidad
-
-        Última actualización: 21/10/2023S
-
-        En [Nombre de tu sitio web] ("Nosotros" o "Nuestro"), respetamos tu privacidad y estamos comprometidos a proteger la
-        información personal que nos proporcionas. Esta Política de Privacidad explica cómo recopilamos, usamos y protegemos
-        tus datos personales.
-
-        1. Recopilación de Información Personal
-
-        Cuando utilizas nuestro sitio web, solo recopilamos información personal si decides proporcionarla voluntariamente.
-        Los tipos de información que recopilamos incluyen:
-
-        Nombre
-        Dirección de correo electrónico
-        Esta información se recopila exclusivamente cuando decides contactarnos a través del formulario de contacto en
-        nuestro sitio web.
-
-        2. Uso de la Información Personal
-
-        Utilizamos la información personal que proporcionas únicamente para los siguientes propósitos:
-
-        Responder a tus consultas o solicitudes.
-        Enviarte comunicaciones relacionadas con tu consulta.
-        No compartimos tu información personal con terceros, a menos que estemos legalmente obligados a hacerlo.
-
-        3. Cookies
-
-        Utilizamos cookies en nuestro sitio web para mejorar la experiencia del usuario. Al acceder a nuestro sitio y
-        aceptar las cookies, consientes el uso de cookies de acuerdo con nuestra Política de Cookies. Puedes obtener más
-        información sobre las cookies que utilizamos y cómo gestionarlas en nuestra Política de Cookies.
-
-        4. Seguridad de los Datos
-
-        Mantenemos medidas de seguridad para proteger tu información personal contra pérdida, robo y acceso no autorizado.
-        Sin embargo, ten en cuenta que ningún método de transmisión por Internet o almacenamiento electrónico es
-        completamente seguro, y no podemos garantizar la seguridad absoluta de tus datos.
-
-        5. Cambios en la Política de Privacidad
-
-        Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Las actualizaciones se
-        harán efectivas una vez que se publiquen en nuestro sitio web. Te recomendamos revisar esta Política de Privacidad
-        periódicamente para mantenerte informado.
-
-        6. Contacto
-
-        Si tienes alguna pregunta o comentario sobre esta Política de Privacidad, o si deseas ejercer tus derechos de
-        protección de datos, por favor, contáctanos a través de [tu dirección de correo electrónico de contacto].
-
-        Al usar nuestro sitio web, aceptas los términos y condiciones de esta Política de Privacidad. Gracias por confiar en
-        Cosecha del Momento.</div>
+    <div class="w-3/6 mx-auto my-10">
+        <h1 class="text-4xl text-center mb-10">Política de Privacidad</h1>
+        <h2 class="text-xl ml-5 mb-5">Última actualización: 22 de Octubre de 2023</h2>
+        <p class="mb-5">En Cosecha del Momento, respetamos tu privacidad y estamos comprometidos a proteger la
+            información personal que nos proporcionas. Esta Política de Privacidad explica cómo recopilamos, usamos y
+            protegemos
+            tus datos personales.</p>
+        <h2 class="text-xl ml-5 mb-5">1. Recopilación de Información Personal</h2>
+        <p class="mb-5">Cuando utilizas nuestro sitio web, solo recopilamos información personal si decides proporcionarla
+            voluntariamente.</p>
+        <p class="mb-5">Los tipos de información que recopilamos incluyen:</p>
+        <p class="mb-2 ml-5">Nombre</p>
+        <p class="mb-5 ml-5">Dirección de correo electrónico</p>
+        <p class="mb-5">Esta información se recopila exclusivamente cuando decides contactarnos a través del formulario de
+            contacto en nuestro sitio web.</p>
+        <h2 class="text-xl ml-5 mb-5">2. Uso de la Información Personal</h2>
+        <p class="mb-5">Utilizamos la información personal que proporcionas únicamente para los siguientes propósitos:</p>
+        <p class="mb-5">Responder a tus consultas o solicitudes.
+            Enviarte comunicaciones relacionadas con tu consulta.
+            No compartimos tu información personal con terceros, a menos que estemos legalmente obligados a hacerlo.</p>
+        <h2 class="text-xl ml-5 mb-5">3. Cookies</h2>
+        <p class="mb-5">Utilizamos cookies en nuestro sitio web para mejorar la experiencia del usuario. Al acceder a
+            nuestro sitio y
+            aceptar las cookies, consientes el uso de cookies de acuerdo con nuestra Política de Cookies. Puedes obtener más
+            información sobre las cookies que utilizamos y cómo gestionarlas en nuestra Política de Cookies.</p>
+        <h2 class="text-xl ml-5 mb-5">4. Seguridad de los Datos</h2>
+        <p class="mb-5">Mantenemos medidas de seguridad para proteger tu información personal contra pérdida, robo y acceso
+            no autorizado.
+            Sin embargo, ten en cuenta que ningún método de transmisión por Internet o almacenamiento electrónico es
+            completamente seguro, y no podemos garantizar la seguridad absoluta de tus datos.</p>
+        <h2 class="text-xl ml-5 mb-5">5. Cambios en la Política de Privacidad</h2>
+        <p class="mb-5">Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Las
+            actualizaciones se
+            harán efectivas una vez que se publiquen en nuestro sitio web. Te recomendamos revisar esta Política de
+            Privacidad
+            periódicamente para mantenerte informado.</p>
+        <h2 class="text-xl ml-5 mb-5">6. Contacto</h2>
+        <p class="mb-5">Si tienes alguna pregunta o comentario sobre esta Política de Privacidad, o si deseas ejercer tus
+            derechos de
+            protección de datos, por favor, contáctanos a través de
+            nuestra sección de
+            <RouterLink to="/contact" class="text-green font-semibold hover:underline">
+                Contacto.
+            </RouterLink>
+        </p>
+        <p>Al usar nuestro sitio web, aceptas los términos y condiciones de esta Política de Privacidad. Gracias por confiar
+            en
+            Cosecha del Momento.</p>
+    </div>
 </template>
 
 <script>
--- src/views/TermsView.vue
+++ src/views/TermsView.vue
@@ -1,63 +1,60 @@
 <template>
-    <div>Términos de Servicio
-
-        Última actualización: [Fecha de la última actualización]
-
-        Bienvenido a [Nombre de tu sitio web] ("Nosotros" o "Nuestro"). Te invitamos a utilizar nuestra plataforma en línea
-        para explorar y aprender sobre frutas y verduras de España. Antes de utilizar nuestros servicios, te pedimos que
-        leas y comprendas estos Términos de Servicio ("Términos") que rigen tu acceso y uso de nuestro sitio web y servicios
-        relacionados.
-
-        1. Aceptación de los Términos
-
-        Al acceder y utilizar nuestro sitio web, aceptas estar legalmente obligado por estos Términos. Si no estás de
-        acuerdo con estos Términos, por favor, no utilices nuestro sitio web.
-
-        2. Uso de Contenido
-
-        2.1. Contenido de la Página
-
-        El contenido de nuestro sitio web, incluyendo la información sobre frutas y verduras de España, como nombres
-        científicos, temporadas, descripciones de origen y comunidades de producción, es proporcionado con fines
-        informativos. Este contenido se basa en datos obtenidos del Ministerio de Agricultura, Pesca y Alimentación y se
-        presenta de buena fe. No garantizamos la exactitud, integridad o actualidad de esta información.
-
-        2.2. Uso Responsable
-
-        Los usuarios son responsables de utilizar el contenido de manera responsable y ética. No está permitido el uso de
-        este sitio web para fines ilegales, difamatorios, obscenos, o cualquier otro propósito que infrinja los derechos de
-        terceros.
-
-        3. Privacidad
-
-        Nuestra política de privacidad describe cómo recopilamos, usamos y protegemos tus datos personales. Te recomendamos
-        que leas nuestra política de privacidad [Enlace a la política de privacidad] para comprender cómo manejamos la
-        información de los usuarios.
-
-        4. Propiedad Intelectual
-
-        Nuestro sitio web y su contenido, incluyendo textos, imágenes, logotipos y marcas registradas, están protegidos por
-        leyes de propiedad intelectual. No se permite la reproducción, distribución o modificación del contenido sin permiso
-        escrito.
-
-        5. Enlaces a Terceros
-
-        Nuestro sitio web puede contener enlaces a sitios web de terceros. No tenemos control sobre el contenido o las
-        políticas de privacidad de estos sitios. No somos responsables de ningún daño o pérdida que puedas sufrir al acceder
-        a estos sitios.
-
-        6. Cambios a los Términos
-
-        Nos reservamos el derecho de modificar estos Términos en cualquier momento. Las actualizaciones se harán efectivas
-        una vez que se publiquen en nuestro sitio web. Te recomendamos revisar estos Términos periódicamente para mantenerte
-        informado.
-
-        7. Contacto
-
-        Si tienes alguna pregunta o comentario sobre estos Términos, por favor, contáctanos a través de [tu dirección de
-        correo electrónico de contacto].
-
-        Gracias por utilizar [Nombre de tu sitio web]. Esperamos que disfrutes de nuestro contenido y servicios.</div>
+    <div class="w-3/6 mx-auto my-10">
+        <h1 class="text-4xl text-center mb-10">Términos de Servicio</h1>
+        <h2 class="text-xl ml-5 mb-5">Última actualización: 22 de Octubre de 2023</h2>
+        <p class="mb-5">Bienvenido a Cosecha del Momento.</p>
+        <p class="mb-5"> Te invitamos a utilizar nuestra plataforma en línea para explorar
+            y aprender
+            sobre frutas y verduras de España. Antes de utilizar nuestros servicios, te pedimos que leas y comprendas estos
+            Términos de Servicio que rigen tu acceso y uso de nuestro sitio web y servicios relacionados.</p>
+        <h2 class="text-xl ml-5 mb-5">1. Aceptación de los Términos</h2>
+        <p class="mb-5">Al acceder y utilizar nuestro sitio web, aceptas estar legalmente obligado por estos Términos. Si no
+            estás de
+            acuerdo con estos Términos, por favor, no utilices nuestro sitio web.</p>
+        <h2 class="text-xl ml-5 mb-5">2. Uso de Contenido</h2>
+        <h3 class="text-xl ml-10 mb-5">2.1. Contenido de la Página</h3>
+        <p class="mb-5">El contenido de nuestro sitio web, incluyendo la información sobre frutas y verduras de España, como
+            nombres
+            científicos, temporadas, descripciones de origen y comunidades de producción, es proporcionado con fines
+            informativos. Este contenido se basa en datos obtenidos del Ministerio de Agricultura, Pesca y Alimentación y se
+            presenta de buena fe. No garantizamos la exactitud, integridad o actualidad de esta información.</p>
+        <h3 class="text-xl ml-10 mb-5">2.2. Uso Responsable</h3>
+        <p class="mb-5">Los usuarios son responsables de utilizar el contenido de manera responsable y ética. No está
+            permitido el uso de
+            este sitio web para fines ilegales, difamatorios, obscenos, o cualquier otro propósito que infrinja los derechos
+            de terceros.</p>
+        <h2 class="text-xl ml-5 mb-5">3. Privacidad</h2>
+        <p class="mb-5">Nuestra política de privacidad describe cómo recopilamos, usamos y protegemos tus datos personales.
+            Te recomendamos que leas nuestra
+            <RouterLink to="/policy" class="text-green font-semibold hover:underline">
+                Política de Privacidad
+            </RouterLink>
+            para comprender cómo manejamos la información de los usuarios.
+        </p>
+        <h2 class="text-xl ml-5 mb-5">4. Propiedad Intelectual</h2>
+        <p class="mb-5">Nuestro sitio web y su contenido, incluyendo textos, imágenes, logotipos y marcas registradas, están
+            protegidos
+            por leyes de propiedad intelectual. No se permite la reproducción, distribución o modificación del contenido sin
+            permiso escrito.</p>
+        <h2 class="text-xl ml-5 mb-5">5. Enlaces a Terceros</h2>
+        <p class="mb-5">Nuestro sitio web puede contener enlaces a sitios web de terceros. No tenemos control sobre el
+            contenido o las
+            políticas de privacidad de estos sitios. No somos responsables de ningún daño o pérdida que puedas sufrir al
+            acceder a estos sitios.</p>
+        <h2 class="text-xl ml-5 mb-5">6. Cambios a los Términos</h2>
+        <p class="mb-5">Nos reservamos el derecho de modificar estos Términos en cualquier momento. Las actualizaciones se
+            harán
+            efectivas una vez que se publiquen en nuestro sitio web. Te recomendamos revisar estos Términos periódicamente
+            para mantenerte informado.</p>
+        <h2 class="text-xl ml-5 mb-5">7. Contacto</h2>
+        <p class="mb-5">Si tienes alguna pregunta o comentario sobre estos Términos, por favor, contáctanos a través de
+            nuestra sección de
+            <RouterLink to="/contact" class="text-green font-semibold hover:underline">
+                Contacto.
+            </RouterLink>
+        </p>
+        <p>Gracias por utilizar Cosecha del Momento. Esperamos que disfrutes de nuestro contenido y servicios.</p>
+    </div>
 </template>
 
 <script>
