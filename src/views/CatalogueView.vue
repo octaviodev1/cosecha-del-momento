133bbd7334ec787f76f00a02747b4dfb98225523
Octavian Badea <octavio.badea@gmail.com>
2023-10-24 12:56:10 +0200

    Fix Se ha centrado el div de No hay Resultado

--- src/views/CatalogueView.vue
+++ src/views/CatalogueView.vue
@@ -9,7 +9,7 @@
     <div v-if="filteredProducts.length" class="flex-1 ml-5 overflow-y-scroll">
       <CatalogueContent :products="filteredProducts" />
     </div>
-    <div v-else>
+    <div v-else class="flex items-center m-auto">
       <div
         class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] font-new-font">
         <h5 class="mb-2 text-2xl font-medium leading-tight text-neutral-800">
