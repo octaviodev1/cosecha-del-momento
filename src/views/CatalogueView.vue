3267bc917efab655df74249ec1210224bd77f8b1
Octavio <octavio.badea@gmail.com>
2023-10-26 21:04:59 +0200

    Fix/Catalogue Se ha arreglado el margin bottom de la seccion de filtros. Se ha añadido un nuevo texto para cuando los datos esten cargando.

--- src/views/CatalogueView.vue
+++ src/views/CatalogueView.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row m-4 h-[88%] gap-16">
 
     <div
-      class="flex flex-col w-[28%] h-[80vh] justify-start ml-4 mb-4 rounded-2xl p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+      class="flex flex-col w-[28%] h-[80vh] justify-start ml-4 mb-14 rounded-2xl p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
       <FiltersSection @filter="filterProducts" @search="searchByText" />
     </div>
 
@@ -11,7 +11,18 @@
       <CatalogueContent :products="filteredProducts" />
     </div>
     <div v-else class="flex items-center m-auto">
-      <div
+      <div v-if="isloading"
+        class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] font-new-font">
+        <h5 class="mb-2 text-2xl font-medium leading-tight text-center">
+          Cargando...
+        </h5>
+        <p class="mb-4 text-base text-center">
+          Espera un momento mientras cargan las frutas y verduras
+        </p>
+        <img src="@/assets/logos/searching.webp" class="m-auto">
+      </div>
+
+      <div v-else
         class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] font-new-font">
         <h5 class="mb-2 text-2xl font-medium leading-tight text-center">
           No hay resultados
@@ -21,6 +32,7 @@
         </p>
         <img src="@/assets/logos/searching.webp" class="m-auto">
       </div>
+
     </div>
   </div>
 </template>
@@ -33,7 +45,8 @@ export default {
   data() {
     return {
       products: [],
-      filteredProducts: []
+      filteredProducts: [],
+      isloading: true
     }
   },
   mounted() {
@@ -41,7 +54,8 @@ export default {
       .then(response => response.json())
       .then(data => {
         this.products = data;
-        this.filteredProducts = this.products
+        this.filteredProducts = this.products;
+        this.isloading = false
       })
       .catch(error => {
         console.error("Error al recuperar los datos: ", error)
